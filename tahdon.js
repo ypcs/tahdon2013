78af63ff1db48a6001e8236696d257f92c1e105e
Ville Korhonen <ville@xd.fi>
2013-03-20 17:00:39 +0200

    EK-puolueiden nimet piiloon

--- tahdon.js
+++ tahdon.js
@@ -104,25 +104,21 @@ $(document).ready(function() {
                 [items[0][0], 118453],
                 [items[items.length - 1][0], 118453]
             ],
-            color: "#0135A5",
-            label: "Kristillisdemokraatit (KD) (vaalit 2011)"
+            label: "Puolue eduskuntavaaleissa 2011"
         },
         {
             data: [
                 [items[0][0], 125785],
                 [items[items.length - 1][0], 125785]
             ],
-            color: "#007ac9",
-            label: "Suomen ruotsalainen kansanpuolue (RKP) (vaalit 2011)"
+            label: "Puolue eduskuntavaaleissa 2011"
         },
         {
             data: [
                 [items[0][0], 213172],
                 [items[items.length - 1][0], 213172]
             ],
-            color: "#61bf1a",
-            label: "Vihreä liitto (Vihr) (vaalit 2011)"
-        },
+            label: "Puolue eduskuntavaaleissa 2011"        },
         {
             data: [
                 [items[0][0], 69381],
